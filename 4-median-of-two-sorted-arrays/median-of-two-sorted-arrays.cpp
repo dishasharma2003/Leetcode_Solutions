4c37195f0b2dd999ac242508707b2851585b5bb4
Disha Sharma <120232342+dishasharma2003@users.noreply.github.com>
2024-02-03 23:53:59 +0530

    Time: 24 ms (68.97%) | Memory: 94.5 MB (14.84%) - LeetSync

---
+++ 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.cpp
@@ -0,0 +1,40 @@
+class Solution {
+public:
+    double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
+        int n1 = nums1.size();
+        int n2 = nums2.size();
+        // Ensure nums1 is the smaller array
+        if (n1 > n2) {
+            swap(nums1, nums2);
+            swap(n1, n2);
+        }
+
+        int low = 0, high = n1;
+
+        while (low <= high) {
+            int partition1 = (low + high) / 2;
+            int partition2 = (n1 + n2 + 1) / 2 - partition1;
+
+            int maxLeft1 = (partition1 == 0) ? INT_MIN : nums1[partition1 - 1];
+            int minRight1 = (partition1 == n1) ? INT_MAX : nums1[partition1];
+
+            int maxLeft2 = (partition2 == 0) ? INT_MIN : nums2[partition2 - 1];
+            int minRight2 = (partition2 == n2) ? INT_MAX : nums2[partition2];
+
+            if (maxLeft1 <= minRight2 && maxLeft2 <= minRight1) {
+                if ((n1 + n2) % 2 == 0) {
+                    return (max(maxLeft1, maxLeft2) + min(minRight1, minRight2)) / 2.0;
+                } else {
+                    return max(maxLeft1, maxLeft2);
+                }
+            } else if (maxLeft1 > minRight2) {
+                high = partition1 - 1;
+            } else {
+                low = partition1 + 1;
+            }
+        }
+
+        // If the input is not sorted, you may need to handle that case separately
+        return 0.0; 
+    }
+};
